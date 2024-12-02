9d28b0b0daa3f290ed02e7165737964fcd1fd434
Alexander Williams <awill@opentitan.org>
2024-12-02 20:43:08 +0100

    [pinmux] Uniquify VLNVs and clean core organization
    Make all core files have instance-unique VLNVs.
    Move packages referenced by other IPs to leaf cores in the
    dependency tree.
    Add a template parameter to specify the real top_pkg and
    scan_role_pkg.
    
    Co-authored-by: Andreas Kurth <adk@lowrisc.org>
    Signed-off-by: Alexander Williams <awill@opentitan.org>

--- util/topgen.py
+++ util/topgen.py
@@ -367,7 +367,9 @@ def generate_pinmux(top: Dict[str, object], out_path: Path) -> None:
         "n_dio_periph_out": n_dio_periph_out,
         "enable_usb_wakeup": pinmux['enable_usb_wakeup'],
         "enable_strap_sampling": pinmux['enable_strap_sampling'],
-        "use_rivos_config": pinmux.get('use_rivos_config', False)
+        "top_pkg_vlnv": f"lowrisc:constants:top_{topname}_top_pkg",
+        "scan_role_pkg_vlnv": f"lowrisc:systems:top_{topname}_scan_role_pkg",
+        "use_rivos_config": pinmux.get('use_rivos_config', False),
     }
 
     ipgen_render("pinmux", topname, params, out_path)
