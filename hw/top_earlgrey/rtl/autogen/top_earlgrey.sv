b5e973f9621aab1aca29b931648a119a47d6af0c
Robert Schilling <rschilling@rivosinc.com>
2024-12-30 02:33:33 -0800

    [hw,i2c,racl] Add RACL support to I2C
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/top_earlgrey/rtl/autogen/top_earlgrey.sv
+++ hw/top_earlgrey/rtl/autogen/top_earlgrey.sv
@@ -1327,6 +1327,9 @@ module top_earlgrey #(
       .ram_cfg_i(ast_ram_1p_cfg),
       .ram_cfg_rsp_o(),
       .lsio_trigger_o(),
+      .racl_policies_i(top_racl_pkg::RACL_POLICY_VEC_DEFAULT),
+      .racl_error_o(),
+      .racl_error_log_o(),
       .tl_i(i2c0_tl_req),
       .tl_o(i2c0_tl_rsp),
 
@@ -1373,6 +1376,9 @@ module top_earlgrey #(
       .ram_cfg_i(ast_ram_1p_cfg),
       .ram_cfg_rsp_o(),
       .lsio_trigger_o(),
+      .racl_policies_i(top_racl_pkg::RACL_POLICY_VEC_DEFAULT),
+      .racl_error_o(),
+      .racl_error_log_o(),
       .tl_i(i2c1_tl_req),
       .tl_o(i2c1_tl_rsp),
 
@@ -1419,6 +1425,9 @@ module top_earlgrey #(
       .ram_cfg_i(ast_ram_1p_cfg),
       .ram_cfg_rsp_o(),
       .lsio_trigger_o(),
+      .racl_policies_i(top_racl_pkg::RACL_POLICY_VEC_DEFAULT),
+      .racl_error_o(),
+      .racl_error_log_o(),
       .tl_i(i2c2_tl_req),
       .tl_o(i2c2_tl_rsp),
 
