4b1950db03dc26c0a507cf7c44f4ceea39b5d17e
Robert Schilling <rschilling@rivosinc.com>
2024-12-30 02:14:59 -0800

    [hw,uart,racl] Add RACL support to UART
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/top_earlgrey/rtl/autogen/top_earlgrey.sv
+++ hw/top_earlgrey/rtl/autogen/top_earlgrey.sv
@@ -1091,6 +1091,9 @@ module top_earlgrey #(
 
       // Inter-module signals
       .lsio_trigger_o(),
+      .racl_policies_i(top_racl_pkg::RACL_POLICY_VEC_DEFAULT),
+      .racl_error_o(),
+      .racl_error_log_o(),
       .tl_i(uart0_tl_req),
       .tl_o(uart0_tl_rsp),
 
@@ -1125,6 +1128,9 @@ module top_earlgrey #(
 
       // Inter-module signals
       .lsio_trigger_o(),
+      .racl_policies_i(top_racl_pkg::RACL_POLICY_VEC_DEFAULT),
+      .racl_error_o(),
+      .racl_error_log_o(),
       .tl_i(uart1_tl_req),
       .tl_o(uart1_tl_rsp),
 
@@ -1159,6 +1165,9 @@ module top_earlgrey #(
 
       // Inter-module signals
       .lsio_trigger_o(),
+      .racl_policies_i(top_racl_pkg::RACL_POLICY_VEC_DEFAULT),
+      .racl_error_o(),
+      .racl_error_log_o(),
       .tl_i(uart2_tl_req),
       .tl_o(uart2_tl_rsp),
 
@@ -1193,6 +1202,9 @@ module top_earlgrey #(
 
       // Inter-module signals
       .lsio_trigger_o(),
+      .racl_policies_i(top_racl_pkg::RACL_POLICY_VEC_DEFAULT),
+      .racl_error_o(),
+      .racl_error_log_o(),
       .tl_i(uart3_tl_req),
       .tl_o(uart3_tl_rsp),
 
