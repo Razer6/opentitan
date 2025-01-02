21dc52083f258250af24860bda6f60e1b27e8657
Robert Schilling <rschilling@rivosinc.com>
2025-01-02 06:18:08 -0800

    [hw,aon_timer,rtk] Add vexpand footer
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/ip/aon_timer/rtl/aon_timer.sv
+++ hw/ip/aon_timer/rtl/aon_timer.sv
@@ -267,3 +267,9 @@ module aon_timer import aon_timer_reg_pkg::*;
   // Alert assertions for reg_we onehot check
   `ASSERT_PRIM_REG_WE_ONEHOT_ERROR_TRIGGER_ALERT(RegWeOnehotCheck_A, u_reg, alert_tx_o[0])
 endmodule
+// Local Variables:
+// fill-column:1
+// verilog-auto-arg-sort:t
+// verilog-typedef-regexp: "_[teu]$"
+// verilog-library-directories:(".")
+// End:
