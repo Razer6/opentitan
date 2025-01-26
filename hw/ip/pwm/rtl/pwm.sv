fb1693b2c8def7be30edd2de221fc1b03fe902c9
Robert Schilling <rschilling@rivosinc.com>
2025-01-26 12:26:34 -0800

    [hw,pwm,racl] Adapt len of RACL vector after downstream changes
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/ip/pwm/rtl/pwm.sv
+++ hw/ip/pwm/rtl/pwm.sv
@@ -10,7 +10,7 @@ module pwm
   parameter logic [NumAlerts-1:0] AlertAsyncOn         = {NumAlerts{1'b1}},
   parameter bit                   EnableRacl           = 1'b0,
   parameter bit                   RaclErrorRsp         = 1'b1,
-  parameter int unsigned          RaclPolicySelVec[23] = '{23{0}},
+  parameter int unsigned          RaclPolicySelVec[29] = '{29{0}},
   parameter int                   PhaseCntDw           = 16,
   parameter int                   BeatCntDw            = 27
 ) (
