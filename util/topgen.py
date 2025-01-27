8e4590f828d4bf9b9e07e0b062a76619d261e5e5
David Schrammel <davidschrammel@rivosinc.com>
2025-01-27 16:37:58 +0100

    [racl] Add RACL support for windows
    Signed-off-by: David Schrammel <davidschrammel@rivosinc.com>

--- util/topgen.py
+++ util/topgen.py
@@ -606,7 +606,7 @@ def generate_racl(topcfg: Dict[str, object], name_to_block: Dict[str, IpBlock],
     # Generate the RACL mappings for all subscribing IPs
     for m in topcfg['module']:
         for if_name, mapping_path in m.get('racl_mappings', {}).items():
-            parsed_register_mapping, racl_group, _ = parse_racl_mapping(
+            parsed_register_mapping, parsed_window_mapping, racl_group, _ = parse_racl_mapping(
                 topcfg['racl'],
                 mapping_path,
                 if_name,
@@ -614,6 +614,7 @@ def generate_racl(topcfg: Dict[str, object], name_to_block: Dict[str, IpBlock],
             m['racl_mappings'][if_name] = {
                 'racl_group': racl_group,
                 'register_mapping': parsed_register_mapping,
+                'window_mapping': parsed_window_mapping,
             }
 
     log.info('Generating RACL Control IP with ipgen')
