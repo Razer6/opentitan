0c2687f2cddf9b2120c6fbc4e803f8de468709d6
Robert Schilling <rschilling@rivosinc.com>
2025-02-03 23:11:24 -0800

    [hw,gpio,rtl] Add vexpand footer
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/ip/gpio/rtl/gpio.sv
+++ hw/ip/gpio/rtl/gpio.sv
@@ -252,3 +252,9 @@ module gpio
   `ASSERT_PRIM_REG_WE_ONEHOT_ERROR_TRIGGER_ALERT(RegWeOnehotCheck_A, u_reg, alert_tx_o[0])
 
 endmodule
+// Local Variables:
+// fill-column:1
+// verilog-auto-arg-sort:t
+// verilog-typedef-regexp: "_[teu]$"
+// verilog-library-directories:(".")
+// End:
