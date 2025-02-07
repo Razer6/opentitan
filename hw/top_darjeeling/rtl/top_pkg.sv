73a966aea83df23555447d3646e22894fe3c612e
Robert Schilling <rschilling@rivosinc.com>
2025-02-07 10:04:34 -0800

    [hw,darjeeling] Change TLUL AIW (a_user width) from 8 to 16
    to accomdate for all hosts.
    
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/top_darjeeling/rtl/top_pkg.sv
+++ hw/top_darjeeling/rtl/top_pkg.sv
@@ -6,8 +6,12 @@ package top_pkg;
 
 localparam int TL_AW=32;
 localparam int TL_DW=32;    // = TL_DBW * 8; TL_DBW must be a power-of-two
-localparam int TL_AIW=8;    // a_source, d_source
-localparam int TL_DIW=1;    // d_sink
+// Rivos change TL_AIW(8) -> 16
+// To accomodate the large number of hosts in SCS
+// TL_DIW(1) -> 16
+// FIXME: We probably can change that back to 1 as OT only passes that through code
+localparam int TL_AIW=16;    // a_source, d_source
+localparam int TL_DIW=16;    // d_sink
 localparam int TL_AUW=28;   // a_user
 localparam int TL_DUW=14;   // d_user
 localparam int TL_DBW=(TL_DW>>3);
