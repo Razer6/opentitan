756fb176f5369fda76851904529f400558019b4e
David Schrammel <davidschrammel@rivosinc.com>
2025-03-04 10:46:39 -0800

    RIVOS: Use ipgen for our custom racl_ctrls
    Change-Id: I0412b3fc4669b03ce012bc608e613aa5c8e0a85f
    Signed-off-by: David Schrammel <davidschrammel@rivosinc.com>

--- generated_ips/generate_ips.sh
+++ generated_ips/generate_ips.sh
@@ -2,6 +2,9 @@
 # Copyright lowRISC contributors.
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
+set -euo pipefail
+
+cd "$(dirname "$0")"
 
 rm -rf autogen/lio_rv_plic/
 ../util/ipgen.py generate \
@@ -18,3 +21,12 @@ rm -rf autogen/lio_alert_handler/
 rm -rf autogen/lio_alert_handler/doc
 rm -rf autogen/lio_alert_handler/fpv
 rm autogen/lio_alert_handler/README.md
+
+for RACLGROUP in lioGrpA lioGrpB lioGrpC lioGrpD ; do
+  rm -rf autogen/racl_ctrl_${RACLGROUP}/
+  ./generate_racl_ctrl.py --racl-config ../hw/top_darjeeling/data/rivos-racl/racl.hjson --racl-group ${RACLGROUP} --instance-name racl_ctrl_${RACLGROUP} --output data/racl_ctrl_${RACLGROUP}_cfg.hjson
+  ../util/ipgen.py generate -C ../hw/ip_templates/racl_ctrl -c data/racl_ctrl_${RACLGROUP}_cfg.hjson -o autogen/racl_ctrl_${RACLGROUP}
+  rm -rf autogen/racl_ctrl_${RACLGROUP}/doc
+  rm -rf autogen/racl_ctrl_${RACLGROUP}/fpv
+  rm -f autogen/racl_ctrl_${RACLGROUP}/README.md
+done
---
+++ generated_ips/generate_racl_ctrl.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+
+import argparse
+import hjson
+import sys
+
+sys.path.append('../util')
+from raclgen.lib import parse_racl_config
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--racl-config',
+                        '-r',
+                        required=True,
+                        help='Path to RACL config hjson file.')
+
+    parser.add_argument('--racl-group',
+                        '-g',
+                        required=True,
+                        help='RACL group.')
+
+    parser.add_argument('--instance-name',
+                        '-n',
+                        required=True,
+                        help='Module instance name.')
+
+    parser.add_argument('--output',
+                        '-o',
+                        required=True,
+                        help='Output filename for ipconfig hjson file.')
+
+    args = parser.parse_args()
+
+    try:
+        racl_config = parse_racl_config(args.racl_config)
+    except ValueError as err:
+        raise SystemExit(f'Failed to parse RACL config "{args.racl_config}". Error: {err}')
+
+    racl_policies = racl_config['policies'].get(args.racl_group)
+    if racl_policies is None:
+        raise SystemExit(f'RACL group {args.racl_group} not defined in RACL config')
+
+    params = {
+        "module_instance_name": args.instance_name,
+        "nr_role_bits":         racl_config["nr_role_bits"],
+        "nr_ctn_uid_bits":      racl_config["nr_ctn_uid_bits"],
+        "nr_policies":          len(racl_policies),
+        'nr_subscribing_ips':   1,
+        "policies":             racl_policies,
+    }
+    ipconfig = {
+        "instance_name":        args.instance_name,
+        "param_values":         params,
+    }
+
+    with open(args.output, "w") as f:
+        f.write(hjson.dumps(ipconfig))
+
+
+if __name__ == '__main__':
+    main()
