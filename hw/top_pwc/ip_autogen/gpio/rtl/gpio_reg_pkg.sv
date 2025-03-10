1c1b082bd9377d4967429350b066c26d30a5dcb8
Robert Schilling <rschilling@rivosinc.com>
2025-03-10 01:14:31 -0700

    [regen] Re-geenrate collateral
    Change-Id: I1046bb68a947ca2f8ad822a0c58ddf9041fb16ba
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/top_pwc/ip_autogen/gpio/rtl/gpio_reg_pkg.sv
+++ hw/top_pwc/ip_autogen/gpio/rtl/gpio_reg_pkg.sv
@@ -7,6 +7,8 @@
 package gpio_reg_pkg;
 
   // Param list
+  parameter int NumIOs = 32;
+  parameter int NumInpPeriodCounters = 0;
   parameter int NumAlerts = 1;
 
   // Address widths within the block
