a9b6b6140290bc92db00f02bd7efe192d2c35a3a
Robert Schilling <rschilling@rivosinc.com>
2025-03-20 01:33:56 -0700

    [sram] Disable sram output flop
    This avoids read collision issues described in:
    https://rivosinc.atlassian.net/browse/RVSDS-5007
    
    Change-Id: I53228c01b36df4592edf2e335c053d13708313af
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/top_pwc/rtl/autogen/top_pwc.sv
+++ hw/top_pwc/rtl/autogen/top_pwc.sv
@@ -25,7 +25,7 @@ module top_pwc #(
   parameter int SramCtrlRetAonNumRamInst = 1,
   parameter bit SramCtrlRetAonInstrExec = 0,
   parameter int SramCtrlRetAonNumPrinceRoundsHalf = 3,
-  parameter bit SramCtrlRetAonFlopRamOutput = 1,
+  parameter bit SramCtrlRetAonFlopRamOutput = 0,
   parameter bit SramCtrlRetAonEccCorrection = 1,
   parameter int SramCtrlRetAonRaclPolicySelRangesRamNum = 1,
   // parameters for rv_dm
@@ -39,7 +39,7 @@ module top_pwc #(
   parameter int SramCtrlMainNumRamInst = 1,
   parameter bit SramCtrlMainInstrExec = 1,
   parameter int SramCtrlMainNumPrinceRoundsHalf = 3,
-  parameter bit SramCtrlMainFlopRamOutput = 1,
+  parameter bit SramCtrlMainFlopRamOutput = 0,
   parameter bit SramCtrlMainEccCorrection = 1,
   parameter int SramCtrlMainRaclPolicySelRangesRamNum = 1,
   // parameters for sram_ctrl_mbox
@@ -47,7 +47,7 @@ module top_pwc #(
   parameter int SramCtrlMboxNumRamInst = 1,
   parameter bit SramCtrlMboxInstrExec = 0,
   parameter int SramCtrlMboxNumPrinceRoundsHalf = 3,
-  parameter bit SramCtrlMboxFlopRamOutput = 1,
+  parameter bit SramCtrlMboxFlopRamOutput = 0,
   parameter bit SramCtrlMboxEccCorrection = 1,
   parameter int SramCtrlMboxRaclPolicySelRangesRamNum = 1,
   // parameters for dma
