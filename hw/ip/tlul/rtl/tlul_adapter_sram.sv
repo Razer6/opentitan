ba56564e78e7b856236afdf26dcb03852bb6129c
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-05-19 22:03:51 +0100

    [prim,rtl] Add a NeverClears parameter to prim_fifo_sync(_cnt)
    This can be used by instantiations to assert that the clr_i port is
    never true (passing that information to the prim_fifo_sync_cnt and
    then the prim_count inside).
    
    The commit is quite large, but that's because there are lots of
    instantiation sites. If one of these parameters is passed wrongly, the
    result will be caught immediately at simulation (from the
    NeverClears_A assertion that's added to prim_fifo_sync or the
    ClrNeverTrue_A assertion that this turns on inside prim_count).
    
    Change-Id: I54cef021a4f5ba29cf6a0e46e12702e1aa2eac50
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/tlul/rtl/tlul_adapter_sram.sv
+++ hw/ip/tlul/rtl/tlul_adapter_sram.sv
@@ -557,10 +557,11 @@ module tlul_adapter_sram
   //    is write op, it is safe to return the response right away. If it is
   //    read request, then D response is waiting until read data arrives.
   prim_fifo_sync #(
-    .Width   (ReqFifoWidth),
-    .Pass    (1'b0),
-    .Depth   (Outstanding),
-    .Secure  (SecFifoPtr)
+    .Width       (ReqFifoWidth),
+    .Pass        (1'b0),
+    .Depth       (Outstanding),
+    .NeverClears (1'b1),
+    .Secure      (SecFifoPtr)
   ) u_reqfifo (
     .clk_i,
     .rst_ni,
@@ -584,6 +585,7 @@ module tlul_adapter_sram
     .Width             (SramReqFifoWidth),
     .Pass              (1'b0),
     .Depth             (Outstanding),
+    .NeverClears       (1'b1),
     .Secure            (SecFifoPtr),
     .OutputZeroIfEmpty (1)
   ) u_sramreqfifo (
@@ -615,10 +617,11 @@ module tlul_adapter_sram
   //    lose the data from the SRAM interface. Remember, SRAM interface doesn't
   //    have back-pressure signal such as read_ready.
   prim_fifo_sync #(
-    .Width   (RspFifoWidth),
-    .Pass    (1'b1),
-    .Depth   (Outstanding),
-    .Secure  (SecFifoPtr)
+    .Width       (RspFifoWidth),
+    .Pass        (1'b1),
+    .Depth       (Outstanding),
+    .NeverClears (1'b1),
+    .Secure      (SecFifoPtr)
   ) u_rspfifo (
     .clk_i,
     .rst_ni,
