a27f6dad1c384619d28d5e6b95ac64ba397f634d
Neal Moyer <neal@rivosinc.com>
2025-06-17 10:13:08 -0700

    update for ot mem backdoor util
    Change-Id: Icce811494c5367f6cfbfae69a8b8c2ca4042b769

--- hw/dv/sv/mem_bkdr_util/rivos_mem_bkdr_util_row_adapter.sv
+++ hw/dv/sv/mem_bkdr_util/rivos_mem_bkdr_util_row_adapter.sv
@@ -71,7 +71,7 @@ class rivos_mem_bkdr_util_row_adapter extends mem_bkdr_util_row_adapter;
       for (int b = 0; b < bits_per_subword; b++) begin
           int true_col_pos;
           int intrlv_col_pos;
-          intrlv_col_pos       = (subword_idx + (((b * 4) + 1) * 4));
+          intrlv_col_pos       = (subword_idx + (((b * 2) + 1) * 4));
           true_col_pos         = (subword_idx * bits_per_subword) + b;
           data[intrlv_col_pos] = write_data[true_col_pos];
       end
