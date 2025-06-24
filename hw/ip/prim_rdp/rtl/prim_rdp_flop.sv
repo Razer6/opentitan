a3f4997e26ce25497575e17dbef4cd67662f3f88
Robert Schilling <rschilling@rivosinc.com>
2025-06-24 00:39:52 -0700

    [prim_rdp] Add flop parameter to prim_flop and prim_flop_en
    Change-Id: I3d73474bd84186f315f2a3a984113a11b9db2bb1
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/ip/prim_rdp/rtl/prim_rdp_flop.sv
+++ hw/ip/prim_rdp/rtl/prim_rdp_flop.sv
@@ -6,6 +6,9 @@
 
 module prim_rdp_flop #(
   parameter int               Width      = 1,
+  // Depth of the flop, used for pipelining. Values >= 1 lead to the corresponding number of flop
+  // stages, connected in series. Values < 1 lead `q_o` to be directly connected to `d_i`.
+  parameter int               Depth      = 1,
   parameter logic [Width-1:0] ResetValue = 0
 ) (
   input                    clk_i,
@@ -14,44 +17,63 @@ module prim_rdp_flop #(
   output logic [Width-1:0] q_o
 );
 
-import rdplib_pkg::*;
-genvar g_i;
+  import rdplib_pkg::*;
 
-generate
-  for(g_i=0;g_i<Width;g_i++) begin : gen_rdp_gate_aff
-    if(ResetValue[g_i]) begin : gen_pos_reset
-      RDP_GATE_AFFS
-        #(
-          .STATUS(SIZE_ONLY)
-        )
-        prim_aff
-        (
-          .clk   (clk_i),      ///< posedge triggered clock
-          .rst_l (rst_ni),     ///< active low async reset
-          //
-          .d     (d_i[g_i]),   ///< d-input
-          .si    (1'b0),       ///< scan-in (typically 1'b0)
-          .se    (1'b0),       ///< scan-enable (typically 1'b0)
-          .q     (q_o[g_i])    ///< q-output
-        );
-    end
-    else begin : gen_neg_reset
-      RDP_GATE_AFFR
-        #(
-          .STATUS(SIZE_ONLY)
-        )
-        prim_aff
-        (
-          .clk   (clk_i),      ///< posedge triggered clock
-          .rst_l (rst_ni),     ///< active low async reset
-          //
-          .d     (d_i[g_i]),   ///< d-input
-          .si    (1'b0),       ///< scan-in (typically 1'b0)
-          .se    (1'b0),       ///< scan-enable (typically 1'b0)
-          .q     (q_o[g_i])    ///< q-output
-        );
+  if (Depth <= 0) begin : gen_fallthrough
+    assign q_o = d_i;
+
+    logic unused_signals;
+    assign unused_signals = ^{clk_i, rst_ni};
+  end else begin : gen_flops
+    logic [Depth-1:0][Width-1:0] d, q;
+
+    // Connect D of first flop to input.
+    assign d[0] = d_i;
+
+    // Connect Q of last flop to output.
+    assign q_o = q[Depth-1];
+
+    for (genvar j = 0; j < Depth; j++) begin : gen_depth
+      for(genvar i = 0; i < Width; i++) begin : gen_rdp_gate_aff
+        if(ResetValue[i]) begin : gen_pos_reset
+          RDP_GATE_AFFS
+            #(
+              .STATUS(SIZE_ONLY)
+            )
+            prim_aff
+            (
+              .clk   (clk_i),      ///< posedge triggered clock
+              .rst_l (rst_ni),     ///< active low async reset
+              //
+              .d     (d[j][i]),   ///< d-input
+              .si    (1'b0),       ///< scan-in (typically 1'b0)
+              .se    (1'b0),       ///< scan-enable (typically 1'b0)
+              .q     (q[j][i])    ///< q-output
+            );
+        end
+        else begin : gen_neg_reset
+          RDP_GATE_AFFR
+            #(
+              .STATUS(SIZE_ONLY)
+            )
+            prim_aff
+            (
+              .clk   (clk_i),      ///< posedge triggered clock
+              .rst_l (rst_ni),     ///< active low async reset
+              //
+              .d     (d[j][i]),   ///< d-input
+              .si    (1'b0),       ///< scan-in (typically 1'b0)
+              .se    (1'b0),       ///< scan-enable (typically 1'b0)
+              .q     (q[j][i])    ///< q-output
+            );
+        end
+      end
+
+      if (j > 0) begin : gen_connect_d
+        // Connect D of current flop to Q of previous flop.
+        assign d[j] = q[j-1];
+      end
     end
   end
-endgenerate
 
 endmodule
--- hw/ip/prim_rdp/rtl/prim_rdp_flop_en.sv
+++ hw/ip/prim_rdp/rtl/prim_rdp_flop_en.sv
@@ -6,6 +6,12 @@
 
 module prim_rdp_flop_en #(
   parameter int               Width      = 1,
+  // Depth of the flop, used for pipelining. Values >= 1 lead to the corresponding number of flop
+  // stages, connected in series. The `en_i` input determines when the *first* flop stage gets
+  // updated, all other flop stages are updated in subsequent clock cycles as the data propagates
+  // through the stages (the `en_i` input doesn't need to be high for that propagation). Parameter
+  // values < 1 lead `q_o` to be directly connected to `d_i`.
+  parameter int               Depth = 1,
   parameter bit               EnSecBuf   = 0,
   parameter logic [Width-1:0] ResetValue = 0
 ) (
@@ -15,72 +21,76 @@ module prim_rdp_flop_en #(
   input        [Width-1:0] d_i,
   output logic [Width-1:0] q_o
 );
+  import rdplib_pkg::*;
 
-import rdplib_pkg::*;
-genvar g_i;
+  logic en;
 
-logic en;
-logic clk;
-
-if (EnSecBuf) begin : gen_en_sec_buf
-  prim_sec_anchor_buf #(
-    .Width(1)
-  ) u_en_buf (
-    .in_i(en_i),
-    .out_o(en)
-  );
-end else begin : gen_en_no_sec_buf
-  assign en = en_i;
-end
-
-RDP_GATE_CKG
-  #(
-    .STATUS(SIZE_ONLY)
-  )
-  prim_aff_gate
-  (
-    .clkin (clk_i),
-    .en    (en),
-    .tston (1'b0),
-    .clkout(clk)
-  );
-
-
-generate
-  for(g_i=0;g_i<Width;g_i++) begin : gen_rdp_gate_aff
-    if(ResetValue[g_i]) begin : gen_pos_reset
-      RDP_GATE_AFFS
-        #(
-          .STATUS(SIZE_ONLY)
-        )
-        prim_aff
-        (
-          .clk   (clk),        ///< posedge triggered clock
-          .rst_l (rst_ni),     ///< active low async reset
-          //
-          .d     (d_i[g_i]),   ///< d-input
-          .si    (1'b0),       ///< scan-in (typically 1'b0)
-          .se    (1'b0),       ///< scan-enable (typically 1'b0)
-          .q     (q_o[g_i])    ///< q-output
-        );
-    end
-    else begin  : gen_neg_reset
-      RDP_GATE_AFFR
-        #(
-          .STATUS(SIZE_ONLY)
-        )
-        prim_aff
-        (
-          .clk   (clk),        ///< posedge triggered clock
-          .rst_l (rst_ni),     ///< active low async reset
-          //
-          .d     (d_i[g_i]),   ///< d-input
-          .si    (1'b0),       ///< scan-in (typically 1'b0)
-          .se    (1'b0),       ///< scan-enable (typically 1'b0)
-          .q     (q_o[g_i])    ///< q-output
-        );
-    end
+  if (EnSecBuf) begin : gen_en_sec_buf
+    prim_sec_anchor_buf #(
+      .Width(1)
+    ) u_en_buf (
+      .in_i(en_i),
+      .out_o(en)
+    );
+  end else begin : gen_en_no_sec_buf
+    assign en = en_i;
   end
-endgenerate
+
+  if (Depth <= 0) begin : gen_fallthrough
+    assign q_o = d_i;
+  end else begin : gen_flops
+    logic [Width-1:0] q;
+    logic clk;
+
+    RDP_GATE_CKG #(
+      .STATUS(SIZE_ONLY)
+    ) prim_aff_gate  (
+      .clkin (clk_i),
+      .en    (en),
+      .tston (1'b0),
+      .clkout(clk)
+    );
+
+    // The first stage uses the enabled clock to capture the data
+    for(genvar i = 0; i < Width; i++) begin : gen_rdp_gate_aff
+      if(ResetValue[i]) begin : gen_pos_reset
+        RDP_GATE_AFFS #(
+          .STATUS(SIZE_ONLY)
+        ) prim_aff (
+          .clk   (clk),        ///< posedge triggered clock
+          .rst_l (rst_ni),     ///< active low async reset
+          //
+          .d     (d_i[i]),   ///< d-input
+          .si    (1'b0),       ///< scan-in (typically 1'b0)
+          .se    (1'b0),       ///< scan-enable (typically 1'b0)
+          .q     (q[i])    ///< q-output
+        );
+      end else begin  : gen_neg_reset
+        RDP_GATE_AFFR #(
+          .STATUS(SIZE_ONLY)
+        ) prim_aff (
+          .clk   (clk),        ///< posedge triggered clock
+          .rst_l (rst_ni),     ///< active low async reset
+          //
+          .d     (d_i[i]),   ///< d-input
+          .si    (1'b0),       ///< scan-in (typically 1'b0)
+          .se    (1'b0),       ///< scan-enable (typically 1'b0)
+          .q     (q[i])    ///< q-output
+        );
+      end
+    end
+
+    // Later stages unconditionally propgate first stage
+    prim_rdp_flop #(
+      .Width      ( Width      ),
+      .Depth      ( Depth - 1  ),
+      .ResetValue ( ResetValue )
+    ) u_flop_later_stage (
+      .clk_i,
+      .rst_ni,
+      .d_i      (q),
+      .q_o
+    );
+  end
 
 endmodule
