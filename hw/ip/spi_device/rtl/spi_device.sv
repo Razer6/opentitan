35788a442a4b86cf09919092d7e48be8f93dad21
Elliot Baptist <elliot.baptist@lowrisc.org>
2025-06-27 12:32:21 +0100

    [spi_device,cleanup] Fix typos in comments/docs
    Change-Id: Id542c0bc532b293289356534a4b92268bc554187
    Signed-off-by: Elliot Baptist <elliot.baptist@lowrisc.org>

--- hw/ip/spi_device/rtl/spi_device.sv
+++ hw/ip/spi_device/rtl/spi_device.sv
@@ -298,7 +298,7 @@ module spi_device
   // Synchronous clear of read buffer tracking.
   logic readbuf_clr;
 
-  // Passthrouth config signals
+  // Passthrough config signals
   logic [255:0] cmd_filter;
 
   logic [31:0] addr_swap_mask;
