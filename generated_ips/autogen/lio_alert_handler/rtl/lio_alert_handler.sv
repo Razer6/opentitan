60d0a1df9125f4b7cb4269c7052201a60786c1ee
Robert Schilling <rschilling@rivosinc.com>
2025-07-08 07:03:58 -0700

    [pwc,mio] Regenerate tops
    Change-Id: Ia1ae62fab519fe80055233320d8446e7dd74b0dd
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- generated_ips/autogen/lio_alert_handler/rtl/lio_alert_handler.sv
+++ generated_ips/autogen/lio_alert_handler/rtl/lio_alert_handler.sv
@@ -15,7 +15,7 @@ module lio_alert_handler
   parameter bit          RaclErrorRsp                                 = EnableRacl,
   parameter top_racl_pkg::racl_policy_sel_t RaclPolicySelVec[lio_alert_handler_reg_pkg::NumRegs] = 
     '{lio_alert_handler_reg_pkg::NumRegs{0}},
-  // Number of cycles a differential skew is tolerated on the alert signal
+  // Number of cycles a differential skew is tolerated on the alert and escalation signal
   parameter int unsigned AlertSkewCycles = 1,
   parameter int EscNumSeverities = 4,
   parameter int EscPingCountWidth = 16,
