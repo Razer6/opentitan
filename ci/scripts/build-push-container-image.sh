fae681cb4aac028156384ee58f81cc27e3cb8078
Alexandre Sollier <asollier@rivosinc.com>
2025-07-23 14:23:15 +0200

    [RIVOS][ci] Update container image build job
    This updates the CI job that takes care of building and pushing the
    OpenTitan container image by restricting when the image gets pushed to
    the container registry:
    
    * the image tagged with the commit hash is now only pushed when the CI
      job is run from a non-dev branch
    * the image tagged latest is now only pushed when the CI job is run from
      the repository default branch
    
    Additionally, a new image tag is pushed when the CI job is run from a
    non-dev branch. It is named after the name of the branch on which the
    container image was built.
    
    Change-Id: I2b1702ea382b699f1886265c6d33afcfe70da96e
    Signed-off-by: Alexandre Sollier <asollier@rivosinc.com>

---
+++ ci/scripts/build-push-container-image.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# SPDX-FileCopyrightText: Copyright (c) 2025 by Rivos Inc.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# Builds and pushes the OpenTitan container image to Rivos' opentitan-integrated container registry.
+# This script is expected to be run from a GitLab CI job.
+
+set -ex
+
+# Build the container image
+docker build --tag "$CI_REGISTRY_IMAGE:$CI_COMMIT_SHA" --file util/container/Dockerfile .
+
+# Push the image if the job is not running on a dev branch
+if [ -n "$CI_COMMIT_BRANCH" ] && [ "$CI_COMMIT_BRANCH" != "dev/*" ]; then
+    docker push "$CI_REGISTRY_IMAGE:$CI_COMMIT_SHA"
+
+    # Also push an image tagged with the branch name
+    cleaned_branch=$(printf '%s' "$CI_COMMIT_BRANCH" | tr '/' '-')
+
+    docker tag "$CI_REGISTRY_IMAGE:$CI_COMMIT_SHA" "$CI_REGISTRY_IMAGE:$cleaned_branch"
+    docker push "$CI_REGISTRY_IMAGE:$cleaned_branch"
+fi
+
+# Push the latest tag if the job is running on the default branch
+if [ "$CI_COMMIT_BRANCH" = "$CI_DEFAULT_BRANCH" ]; then
+    docker tag "$CI_REGISTRY_IMAGE:$CI_COMMIT_SHA" "$CI_REGISTRY_IMAGE:latest"
+    docker push "$CI_REGISTRY_IMAGE:latest"
+fi
