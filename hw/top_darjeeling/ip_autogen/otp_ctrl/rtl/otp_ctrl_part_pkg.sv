aea96b43e02954cd2cab1ec1c33acfd052ff1c69
Guillermo Maturana <matute@zerorisc.com>
2025-08-14 18:33:29 +0000

    [rtl,otp_ctrl] Remove unused data from otp_broadcast output
    This data only creates busy-work for DV and the toplevel linters, and
    adds useless wires unless synthesis removes them altogether.
    
    Change-Id: I07963c4ae92e2b2c1568223adbaa17c04ba2fa72
    Signed-off-by: Guillermo Maturana <matute@zerorisc.com>

--- hw/top_darjeeling/ip_autogen/otp_ctrl/rtl/otp_ctrl_part_pkg.sv
+++ hw/top_darjeeling/ip_autogen/otp_ctrl/rtl/otp_ctrl_part_pkg.sv
@@ -531,17 +531,14 @@ package otp_ctrl_part_pkg;
 
   // Breakout types for easier access of individual items.
   typedef struct packed {
-    logic [63:0] hw_cfg0_digest;
     logic [255:0] device_id;
   } otp_hw_cfg0_data_t;
 
   // default value used for intermodule
   parameter otp_hw_cfg0_data_t OTP_HW_CFG0_DATA_DEFAULT = '{
-    hw_cfg0_digest: 64'h67BBE3B4555DF35C,
     device_id: 256'h90C7F21F6224F027F98C48B1F93772844A22D4B78FE0266FBEE3958332F2939B
   };
   typedef struct packed {
-    logic [63:0] hw_cfg1_digest;
     logic [47:0] unallocated;
     prim_mubi_pkg::mubi8_t en_sram_ifetch;
     prim_mubi_pkg::mubi8_t en_csrng_sw_app_read;
@@ -549,13 +546,11 @@ package otp_ctrl_part_pkg;
 
   // default value used for intermodule
   parameter otp_hw_cfg1_data_t OTP_HW_CFG1_DATA_DEFAULT = '{
-    hw_cfg1_digest: 64'h6FDFE93D3146B0F,
     unallocated: 48'h0,
     en_sram_ifetch: prim_mubi_pkg::mubi8_t'(8'h69),
     en_csrng_sw_app_read: prim_mubi_pkg::mubi8_t'(8'h69)
   };
   typedef struct packed {
-    logic [63:0] hw_cfg2_digest;
     logic [31:0] unallocated;
     logic [255:0] manuf_state;
     logic [31:0] soc_dbg_state;
@@ -563,7 +558,6 @@ package otp_ctrl_part_pkg;
 
   // default value used for intermodule
   parameter otp_hw_cfg2_data_t OTP_HW_CFG2_DATA_DEFAULT = '{
-    hw_cfg2_digest: 64'h41837480464544A1,
     unallocated: 32'h0,
     manuf_state: 256'h63B9485A3856C417CF7A50A9A91EF7F7B3A5B4421F462370FFF698183664DC7E,
     soc_dbg_state: 32'h0
@@ -585,7 +579,7 @@ package otp_ctrl_part_pkg;
   };
 
 
-  // OTP invalid partition default for buffered partitions.
+  // OTP invalid partition default for all partitions.
   parameter logic [163839:0] PartInvDefault = 163840'({
     704'({
       320'h1136C663A36C3E3E817E760B27AE937BFCDF15A3429452A851B80674A2B6FBE93B61DE417B9FB33,
@@ -613,13 +607,13 @@ package otp_ctrl_part_pkg;
     }),
     384'({
       64'h41837480464544A1,
-      32'h0, // unallocated space
+      32'h0, // unallocated 4 bytes
       256'h63B9485A3856C417CF7A50A9A91EF7F7B3A5B4421F462370FFF698183664DC7E,
       32'h0
     }),
     128'({
       64'h6FDFE93D3146B0F,
-      48'h0, // unallocated space
+      48'h0, // unallocated 6 bytes
       8'h69,
       8'h69
     }),
@@ -639,7 +633,7 @@ package otp_ctrl_part_pkg;
     }),
     94528'({
       64'hC469C593E5DC0DA8,
-      20928'h0, // unallocated space
+      20928'h0, // unallocated 2616 bytes
       73536'h0
     }),
     8192'({
@@ -754,7 +748,7 @@ package otp_ctrl_part_pkg;
     }),
     1472'({
       64'hCC6596C7174EBA64,
-      32'h0, // unallocated space
+      32'h0, // unallocated 4 bytes
       32'h0,
       32'h0,
       32'h0,
@@ -889,6 +883,8 @@ package otp_ctrl_part_pkg;
     return part_access_pre;
   endfunction : named_part_access_pre
 
+  // Create the broadcast data from specific partitions excluding digests since they
+  // are of no use for consumers of this data data.
   function automatic otp_broadcast_t named_broadcast_assign(
       logic [NumPart-1:0] part_init_done,
       logic [$bits(PartInvDefault)/8-1:0][7:0] part_buf_data);
@@ -949,13 +945,16 @@ package otp_ctrl_part_pkg;
                 part_buf_data[SocFusesFtOffset +: SocFusesFtSize]};
     // HW_CFG0
     valid &= part_init_done[HwCfg0Idx];
-    otp_broadcast.hw_cfg0_data = otp_hw_cfg0_data_t'(part_buf_data[HwCfg0Offset +: HwCfg0Size]);
+    otp_broadcast.hw_cfg0_data = otp_hw_cfg0_data_t'(part_buf_data[HwCfg0Offset +: (HwCfg0Size - 8)]);
+    unused ^= ^part_buf_data[HwCfg0Offset + (HwCfg0Size - 8) +: 8];
     // HW_CFG1
     valid &= part_init_done[HwCfg1Idx];
-    otp_broadcast.hw_cfg1_data = otp_hw_cfg1_data_t'(part_buf_data[HwCfg1Offset +: HwCfg1Size]);
+    otp_broadcast.hw_cfg1_data = otp_hw_cfg1_data_t'(part_buf_data[HwCfg1Offset +: (HwCfg1Size - 8)]);
+    unused ^= ^part_buf_data[HwCfg1Offset + (HwCfg1Size - 8) +: 8];
     // HW_CFG2
     valid &= part_init_done[HwCfg2Idx];
-    otp_broadcast.hw_cfg2_data = otp_hw_cfg2_data_t'(part_buf_data[HwCfg2Offset +: HwCfg2Size]);
+    otp_broadcast.hw_cfg2_data = otp_hw_cfg2_data_t'(part_buf_data[HwCfg2Offset +: (HwCfg2Size - 8)]);
+    unused ^= ^part_buf_data[HwCfg2Offset + (HwCfg2Size - 8) +: 8];
     // SECRET0
     unused ^= ^{part_init_done[Secret0Idx],
                 part_buf_data[Secret0Offset +: Secret0Size]};
