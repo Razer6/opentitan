26f415afe1a4fc96c1089b9cefb11ca26d13feb3
Robert Schilling <rschilling@rivosinc.com>
2025-09-25 01:34:14 -0700

    [prim_rdp] Map 2p sram and assert them not being used
    Change-Id: I56a676b0ba7e75477a7be01edc1a7664279440f4
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

---
+++ hw/ip/prim_rdp/rtl/prim_ram_2p.sv
@@ -0,0 +1,41 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Synchronous dual-port SRAM register model
+//   This module is for simulation and small size SRAM.
+//   Implementing ECC should be done inside wrapper not this model.
+`include "prim_assert.sv"
+
+module prim_ram_2p import prim_ram_2p_pkg::*; #(
+  parameter  int Width           = 32, // bit
+  parameter  int Depth           = 128,
+  parameter  int DataBitsPerMask = 1, // Number of data bits per bit of write mask
+  parameter      MemInitFile     = "", // VMEM file to initialize the memory with
+
+  localparam int Aw              = $clog2(Depth)  // derived parameter
+) (
+  input clk_a_i,
+  input clk_b_i,
+
+  input                    a_req_i,
+  input                    a_write_i,
+  input        [Aw-1:0]    a_addr_i,
+  input        [Width-1:0] a_wdata_i,
+  input  logic [Width-1:0] a_wmask_i,
+  output logic [Width-1:0] a_rdata_o,
+
+
+  input                    b_req_i,
+  input                    b_write_i,
+  input        [Aw-1:0]    b_addr_i,
+  input        [Width-1:0] b_wdata_i,
+  input  logic [Width-1:0] b_wmask_i,
+  output logic [Width-1:0] b_rdata_o,
+
+  input  ram_2p_cfg_t      cfg_i,
+  output ram_2p_cfg_rsp_t  cfg_rsp_o
+);
+  // Prim RDP does not support 2p ram, use 1r1w instead
+  `ASSERT_INIT(PrimRdpDoesNotSupport2PRam_A, 0)
+endmodule
