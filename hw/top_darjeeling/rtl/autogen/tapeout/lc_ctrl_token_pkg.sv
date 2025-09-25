35ea3ac77bda53096ea45e2a49f1dbfa04a279a0
Robert Schilling <rschilling@rivosinc.com>
2025-09-25 05:37:17 -0700

    [secrets] Re-generate secret collateral
    Change-Id: I7498506eaac74dbd1bcfc8559e31a1bb9607c505
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

---
+++ hw/top_darjeeling/rtl/autogen/tapeout/lc_ctrl_token_pkg.sv
@@ -0,0 +1,17 @@
+{
+	"data": "ENC[AES256_GCM,data:XL420TaXa3y0TTVh7tzlpDdeTnvjRJ5naoxLldylC/+xXXdoABMYmpWaDq5hBatbmliTaxviqE7R+lYDt1JmHMTos8v9LSh80x13590VnDqLKGlbRgq8iK+v+Mw6wBKD898fMi2Vh1h36yfkQpd+JSahyRdH+0PP/4+4i2nsxW/dCcqDZ9SVqLMmA8JNJabMmlNkJKmi9s5nzg5QAgOSmZY4sA/S2FAv55mmhQxIE6KTsx3E+vsVSaQ8hbARm87MbqqmiJd4zVhuHnH3arlprnDCuUTalV3/aRAxXjeJFjFw3V5Nar/AFi/X68QkmXzy4AEL3QonbONhOljFENixoSg5UmALlv8/4BfiKLqxodYn+vPNHfs6ufDHOTTrQMFInTUqUY2zVD8cEfPtXkVFuBRI2hUV6s/ZK0H+HFH1F2N2aJdxBd3KVGYCSI/U1p2cFiwOCB/n1NQ7HtoiSpTnwEo+kOIH1pKizt+GycQCkaaO7xdOFn0OXKoqycMKGV1i4O4bcV083alzf/e/qW0imefBwwJnat9bGwW2QqkBEoKNMln2h+IiAkB+UgUV5qXwUrmwbpK5YwRrGrfydu+dnt5R49zv9vrMQYup1F0DsVgyj1BcT6gyecuvOp+fWPFfWnKTmlcnJyo7OOfkHpuL7WvHJy1QY0oGu6csaInNrYjuDzyEzr5B5SSkmlfrjun/R+IumcRmdYNGtiABYQQDceNvc5Rcf6PEBgHd1q9eGT1epLit6K5OD9qGtu0MTOfOmbx2AxXXMZP+klxMTAWQ5/wntZ66CtJBOuGtp2SNafNNy+wqwBab0y8+YkZUBlj8kuRG0RH4OW/qOiDo19A7U2xDSymS+IzNivwNiYPBkATyczCSYrblvkl1tsvznO6JqvnB0bfOhc7+jrmljL9n2IE2+Bnincm4DTQDami4cNeSL5o6dxeRFRft2gsD6HKOgNconjrletrGsS/95X3SiCxzXptiilBcgzRXUbW5aXYH+Vytn5rbvnVbQfsAiTebH6fg+x7IUWUixbwPwt/kWtWnkLwkqeh7TblJFZoyiR/t5sZKULquJdfWkktmHm1JG5+c9tY8Of88bNFF32l63I75N7Ve7pf2ZdQmUDWUD4XBKTlPvCmDgv46L+uWAR8WdCk9Mml/rCh8wGn3SwHscDnzlU2Rn3t6sXQBN93jiFFTEujLgb2KvkjWAo69oRR3F2mFfbuK7xylDFN3JHxGzTBUx5YGfmbDBp+OcR/wzz+qw2LS9izfp2mDzDNvmE7mJxhOh7X5I+Sh0g+D,iv:kvUlf3Kd2Pnzpk3YpG4xeNTY8wJ2CxX4KghZnOHU55s=,tag:KE5zBwwdrBS/zeeeDJm8qA==,type:str]",
+	"sops": {
+		"kms": [
+			{
+				"arn": "arn:aws:kms:us-east-2:332666482539:key/8617ec30-d4fa-4cec-8172-c93e0175048d",
+				"created_at": "2025-09-25T12:43:35Z",
+				"enc": "AQICAHjaIbR5cQ5XQ0/lOkRaC9ip/GE/M1KPFQtqfmuFBe8ezQHawnBwO9BxzKK7j0vNDQF3AAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMx590j0xmZ97RRmL9AgEQgDtOlQ1y7imYDrv2dSsDXtI4UCFx/3AWrpgT922U8NWzXTJvGrBcQ2a8FZyYaVTghEQVN3if1S8ewDishg==",
+				"aws_profile": ""
+			}
+		],
+		"lastmodified": "2025-09-25T12:43:35Z",
+		"mac": "ENC[AES256_GCM,data:gJF+Vy8hKDK+NxUpicx2a9/esCDiFCzlsEMed0rg4M30CxNmyCTi6TzXrr2uC/ppY0+TDiU2C7MaGmt1YyL2EtYrWqLylUPDNcWqBi+fRjsmNjeo+NjP2YksbdRyMqJg2SioI858LpOWi4Vq5+aIgVe/KzN9Sr8AJ7f4xd97diQ=,iv:0IABtjs83zEPXNWmDe7HhIYxfcoCFkiTZpBDgWi0Jw8=,tag:5LCvL1aQcSxYJFk/0Ybxjw==,type:str]",
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.10.2"
+	}
+}
