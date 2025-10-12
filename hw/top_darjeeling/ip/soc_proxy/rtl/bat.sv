a1827ecb85d2b0ea4f094cd1c006d4fdd1152ab3
Neal Moyer <neal@rivosinc.com>
2025-10-11 20:14:23 -0700

    change soc_proxy bat logic for CTN 1-3GiB access and respin MIO, PWC xbars
    Change-Id: I9eb9e8e7b160d4536fc2e09be999969e42b080cc

--- hw/top_darjeeling/ip/soc_proxy/rtl/bat.sv
+++ hw/top_darjeeling/ip/soc_proxy/rtl/bat.sv
@@ -17,7 +17,11 @@ module bat (
 
   // A valid request in the range [1G,2G) or a broadcast request is considered to be valid
   logic ctn_request;
-  assign ctn_request = tl_in_h2d_i.a_valid & (tl_in_h2d_i.a_address[31:30] == 2'b01);
+  assign ctn_request = tl_in_h2d_i.a_valid & (tl_in_h2d_i.a_address[31:30] >= 2'b01)
+                                           & (tl_in_h2d_i.a_address[31:30] <  2'b11);
+
+  logic [1:0] post_bat_addr_31_30;
+  assign post_bat_addr_31_30 = tl_in_h2d_i.a_address[31:30] - 2'b01;
 
   // Need to subtract my socket and chip ID's before sending to SCX
   // so that transaction targeting our own SCS instance will stay on our SCX
@@ -28,7 +32,8 @@ module bat (
   assign post_bat_cid = tl_in_h2d_i.a_address[27:26] - integrator_id_i[1:0]; // Chiplet ID
 
   logic broadcast_pwc;
-  assign broadcast_pwc = (tl_in_h2d_i.a_address[25:21] == 5'd31) & 
+  assign broadcast_pwc = (tl_in_h2d_i.a_address[31:30] == 2'd01) &  // decode on pre-bat addr
+                         (tl_in_h2d_i.a_address[25:21] == 5'd31) & 
                          (tl_in_h2d_i.a_address[20:16] == 5'd9);
 
   // up-lift PWC.bcastAddr by 32K, assume there are less than 32K bcast registers
@@ -40,16 +45,16 @@ module bat (
 
   // If there is a valid CTN request, perform the BAT (downlift to 0-1GB),
   // else use the original address
-  assign bat_address = ctn_request ?    // CTN: [1G,2G), indicates wether addr[29:28],
-    {                                   // [27:26] need to be recovered
-      2'b0,                             // Need to zero out [31:30] to shift ibex 1G-2G down to 
-                                        // system 0-1G
-      post_bat_sid,                     // offset'ed SID, to-be-recovered
-      post_bat_cid,                     // offset'ed CID, to-be-recovered
-      tl_in_h2d_i.a_address[25:21],     // SSID is NOT altered, SSID for SCS is looped back by SCX, 
-                                        // otherwise routed to SCS's CTR
-      tl_in_h2d_i.a_address[20:16],     // UID is not altered
-      post_bat_page_id,                 // PageID is altered/recovered if {SSID = 31, UID = 9 = PWC}
+  assign bat_address = ctn_request ?          // CTN: [1G,2G), indicates wether addr[29:28],
+    {                                         // [27:26] need to be recovered
+      post_bat_addr_31_30,                    // Need to zero out [31:30] to shift ibex 1G-3G down to 
+                                              // system 0-2G
+      post_bat_sid,                           // offset'ed SID, to-be-recovered
+      post_bat_cid,                           // offset'ed CID, to-be-recovered
+      tl_in_h2d_i.a_address[25:21],           // SSID is NOT altered, SSID for SCS is looped back by SCX, 
+                                              // otherwise routed to SCS's CTR
+      tl_in_h2d_i.a_address[20:16],           // UID is not altered
+      post_bat_page_id,                       // PageID is altered/recovered if {SSID = 31, UID = 9 = PWC}
       tl_in_h2d_i.a_address[11:0]
     }
     : tl_in_h2d_i.a_address;
