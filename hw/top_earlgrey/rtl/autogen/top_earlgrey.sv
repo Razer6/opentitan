094608f2ddaa2d67deeb264f2cb5e3059ccf9de2
Robert Schilling <rschilling@rivosinc.com>
2025-11-06 13:17:23 +0100

    [topgen,rtl] Fix param handling for AlertSkewCycles for alert_handler
    Previously AlertSkewCycles is only set for modules that have alert_connections.
    However, the alert handler itself does not have alert_connections, so it was not
    getting the AlertSkewCycles parameter set. Thus the alert handler was using the
    default value of 1, causing an inconsistency if the parameter was set to a different
    value.
    
    This commit fixes this by adding AlertSkewCycles to the alert handler parameters.
    
    Change-Id: Iaac183910e86b3530266d0c12114cf884a6fa33b
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/top_earlgrey/rtl/autogen/top_earlgrey.sv
+++ hw/top_earlgrey/rtl/autogen/top_earlgrey.sv
@@ -1700,6 +1700,7 @@ module top_earlgrey #(
       .rst_kmac_ni (rstmgr_aon_resets.rst_lc_n[rstmgr_pkg::Domain0Sel])
   );
   alert_handler #(
+    .AlertSkewCycles(top_pkg::AlertSkewCycles),
     .RndCnstLfsrSeed(RndCnstAlertHandlerLfsrSeed),
     .RndCnstLfsrPerm(RndCnstAlertHandlerLfsrPerm),
     .EscNumSeverities(AlertHandlerEscNumSeverities),
