7a38d21100e152f2b69225ffa6593582e81c4fd9
Robert Schilling <rschilling@rivosinc.com>
2025-11-10 04:37:13 -0800

    [hw,prim_diff_alert_to_diff,rtl] Stretch alert request
    ... to allow propgating through CDC prims
    
    Change-Id: Ice70c0ce77e17ac035c31adf9ce28f9351fdc869
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/ip/prim/rtl/prim_alert_to_diff.sv
+++ hw/ip/prim/rtl/prim_alert_to_diff.sv
@@ -12,11 +12,15 @@
 // encoding (for minimal integrity protection) to modules that do not support
 // OpenTitan's alert protocol.
 
+`include "prim_assert.sv"
+
 module prim_alert_to_diff #(
   // AsyncOn: Enables additional synchronization logic within the alert receiver.
   parameter bit AsyncOn = 1'b0,
   // Number of cycles a differential skew is tolerated on the alert signal
-  parameter int unsigned SkewCycles = 1
+  parameter int unsigned SkewCycles = 1,
+  // Number of cycles to stretch the outgoing alert signal
+  parameter int unsigned StretchCycles = 8
 ) (
   input logic                       clk_i,
   input logic                       rst_ni,
@@ -27,6 +31,8 @@ module prim_alert_to_diff #(
   output logic                      diff_po,
   output logic                      diff_no
 );
+  // Ensure StretchCycles is greater than 0 for shift register
+  `ASSERT_INIT(StretchCycles_A, StretchCycles > 0);
 
   logic integ_error;
   logic alert;
@@ -52,11 +58,30 @@ module prim_alert_to_diff #(
   logic combined_alert;
   assign combined_alert = integ_error | alert;
 
+  // Logic to stretch the combined_alert signal using a shift register.
+  logic [StretchCycles-1:0] stretch_sr_d, stretch_sr_q;
+  logic combined_alert_stretched;
+
+  assign stretch_sr_d = {stretch_sr_q[StretchCycles-2:0], combined_alert};
+
+  prim_flop #(
+    .Width(StretchCycles),
+    .ResetValue('0)
+  ) u_stretch_flop (
+    .clk_i,
+    .rst_ni,
+    .d_i(stretch_sr_d),
+    .q_o(stretch_sr_q)
+  );
+
+  // The stretched output is high if a new alert just came OR any bit in the register is high.
+  assign combined_alert_stretched = |stretch_sr_q | combined_alert;
+
   // Instantiates the differential encoder module.
   prim_diff_encode u_prim_diff_encode (
     .clk_i,
     .rst_ni,
-    .req_i(combined_alert), // Input request signal (combined alert).
+    .req_i(combined_alert_stretched),
     .diff_po,
     .diff_no
   );
